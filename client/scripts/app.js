6f941c9b40292f7e0a27d1eff4d079cf9025d290
Hack Reactor Student <student@hackreactor.com>
2017-07-15 18:55:58 -0700

    almost finished bleach require not work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,11 @@
+
 var app = {
   //resturcture for clarity
   //additional blank or defualt properties that are not methods?
   storage: [], //data structure? Queue? 
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', // info
-  rooms: new Set()
+  rooms: new Set(),
+  currRoom: ''
   //etc.? 
 }; 
 app.init = function() {
@@ -18,11 +20,12 @@ app.send = function(message) {
   //initiates the AJAX request to send message to the server
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: this.server, //app.server?
+    url: app.server, //app.server?
     type: 'POST',
     data: JSON.stringify(message),
-    contentType: 'json',
+    contentType: 'application/json',
     success: function (data) {
+      app.renderRoom(app.currRoom);
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -35,23 +38,26 @@ app.send = function(message) {
 app.fetch = function() { 
   //initiates the AJAX request to pull new messages from the server. 
   
-  
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
-    data: null,
+    data: 'order=-createdAt',
     contentType: 'json',
     success: function (data) {
       console.log('chatterbox: Fetch successful');
       //send data from fetch to storage
       app.storage = data.results;
+      //creat list of room names on fetch
       for (var i = 0; i < app.storage.length; i++) {
+        //var cleanRoom = bleach.sanitize(app.storage[i]);
         if (!app.rooms.has(app.storage[i].roomname)) {
+          
           $('#roomSelect').append(`<option> ${app.storage[i].roomname} </option>`);
         }
         app.rooms.add(app.storage[i].roomname);
       }
+      app.renderRoom(app.currRoom);
       
     },
     error: function (data) {
@@ -73,12 +79,13 @@ app.renderMessage = function(message) { //input will be a reference to a specifi
     //upon click 'clicked' user should be added as a friend
   
   //escaping on data
-  
+  //var cleanUser = bleach.sanitize(message.username);
+  //var cleanMessage = bleach.sanitize(message.text);
   
   $('#chats').append(`
-    <div>
-      <p class="username">${message.username}: </p>
-      <p>${message.text}</p>
+    <div class="chat">
+      <span class="chat username">${message.username}: </span>
+      <span>${message.text}</span>
     </div>`); 
 };
   
@@ -92,12 +99,16 @@ app.renderRoom = function(thisRoom) {
     //if true pass message object to renderMessage
   if (app.rooms.has(thisRoom)) {
     //loop through storage 
-      //call renderMessage on each message with a matching room value
+      //call renderMessage on each message with a matching room value\
+    app.clearMessages();
+    app.currRoom = thisRoom;
     app.storage.filter(message => message.roomname === thisRoom)
                .forEach(message => app.renderMessage(message));
+               //animate message add
   } else {
+    app.currRoom = thisRoom;
     app.rooms.add(thisRoom);
-    $('#roomSelect').append(`<option> ${room} </option>`);
+    $('#roomSelect').append(`<option> ${thisRoom} </option>`);
   } 
 };
   
@@ -105,14 +116,31 @@ app.handleUsernameClick = function () {
   //user name click function body
   // on click add user anme 'clicked' to 'friends'
     //this.addClass('friend')
-  //has own property that reverses change? toggle? 
+  //has own property that reverses change? toggle?
+  
 };
 
 app.handleSubmit = function() {
   //creates message based on input text and user currently running app
   // sets to undefined(as yet) storage property?
   //has additional method that allows the recall/deletion of a message?
+  var message = {};
+  //debugger
+  // capture username room and message
+  // var message = {
+  // username: 'shawndrost',
+  // text: 'trololo',
+  // roomname: '4chan'
+  message.text = $('#message').val();
+  message.username = window.location.search.slice(10);
+  message.roomname = app.currRoom;
+  // bluid message object
+  
+  // call post
+  app.send(message);
+  
 };
+//timeout loop for pulling messages from server
 app.refreshFeed = function() {
   app.fetch();
   setTimeout(app.refreshFeed, 3000);
@@ -122,10 +150,6 @@ $('document').ready(function() {
   //start up on click handlers
   $('#main').on('click', '.username', function(event) {
     app.handleUsernameClick();
-  });
-  // submitHandle trigger
-  $('#send .submit').on('submit', function(event) {
-    app.handleSubmit(event);
   });
   //click handler for 'add room' 
   $('#roomSelect').change(function() {
@@ -138,67 +162,17 @@ $('document').ready(function() {
       }
     } else {
       app.renderRoom($(this).val());
+      
     }
 
   });
+  //handler for sending messages
+  document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+    app.handleSubmit();
+    console.log('that worked!');
+  });
+  //app.init();
   
-  //app.refreshFeed();
-  
-   
-  
-  
-  /*
-  function displayLastTweets(){
-         var lastIndex = streams.home.length - 1;
-         index = indexLastTweetDiplayed;
-         //Update nbr of tweet received
-         $('#nbrTweet').text(`${streams.home.length} tweetls received`);
-         while(index <= lastIndex){
-           if ((filterName !== undefined && streams.home[index].user === filterName) || filterName === undefined){
-             var tweet = streams.home[index];
-             var $tweet = $('<div class="tweet"></div>');
-             var tweetDate = '<span class="dateTweet">' + tweet.created_at.toDateString() + ' ' + tweet.created_at.toLocaleTimeString() + '</span>';
-             var user = '<span ><strong class="userName">' + tweet.user + '</strong></span>';
-             $tweet.html( tweetDate + ' @' + user + ': ' + hashtag(tweet.message));
-             $tweet.prependTo('#lastTweet');
-           }
-           index += 1;
-         }
-         indexLastTweetDiplayed = index;
-         //filter on user name tweet displayed if user name is clicked
-
-         setTimeout(displayLastTweets, 5000);
-       };
-  */
-  
-  
+  app.refreshFeed();
 });
-
-
-/*
-app.prototype.init = function() {
-  return true;
-};
-*/
-
-/*var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};// YOUR CODE HERE:
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-*/
